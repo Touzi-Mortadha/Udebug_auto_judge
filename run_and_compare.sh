cfdd612450db726f044c7f6e27437d858689527f
touzi_mortadha <mortadha.touzi@predictix.com>
2018-04-01 09:25:20 +0100

    add first test

---
+++ run_and_compare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+g++ -std=c++11 -DONLINE_JUDGE main.cpp -o main
+
+cat input.txt | ./main > my_out.txt
+# if(diff my_out.txt output.txt); then
+#   echo "yes"
+# else
+#   echo "no"
+# fi
